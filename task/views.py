7f669812942db4257f96d551127a7e1589a49c67
Nadim <hussennadim49@gmail.com>
2022-06-19 20:46:13 +0600

    task show 🥳🥳🥳🥳🥳🥳🥳🥳🥳

--- task/views.py
+++ task/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+tasks = ['foo','woe','bar','baz','all','is','well']
 def index(request):
-    return render(request, "task.html")
+    return render(request, "task.html",{
+        'tasks':tasks
+    })
