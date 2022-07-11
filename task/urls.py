606f64ddbed496b506b2b13d34744523685d69ee
Nadim <hussennadim49@gmail.com>
2022-07-11 19:35:57 +0600

    upload and fetch but error in image upload 😥😥😥

--- task/urls.py
+++ task/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path("task",views.index, name="index"),
+    path("insert_Product",views.insert),
     # path("<str:name>",views.greeting, name="greeting"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.contrib import messages
+from itertools import product
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
-tasks = ['foo','woe','bar','baz','all','is','well']
+# tasks = ['foo','woe','bar','baz','all','is','well']
 def index(request):
-    return render(request, "task.html",{
-        'tasks':tasks
-    })
+    products = Item.objects.all()
+    context = {
+        'product':products
+    }
+    return render(request, "task.html",context)
+
+def insert(request):
+    if request.method == "POST":
+        prod = Item()
+        prod.name = request.POST.get('name')
+        prod.price = request.POST.get('price')
+        prod.description = request.POST.get('description')
+
+        if len(request.FILES) !=0:
+            prod.image = request.FILES['image']
+        prod.save()
+        messages.success(request, "product added successfully")
+        return redirect('/task/task')
+
+    return render(request,'insertProduct.html')
